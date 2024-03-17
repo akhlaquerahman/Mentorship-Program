a2acea4edaae94d48b7fc3ecce22d9a243744dc3
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-18 00:09:58 +0530

    print odd number 1 to 100

---
+++ 14 March(PROBLEMS ON LOOP)/checkNumberPrimeOrNut.cpp
@@ -0,0 +1,7 @@
+                       // WAP to check if a number is prime or not (use of break statement)
+#include<iostream>
+using namespace std;
+main(){
+    int a;
+    
+}                       
