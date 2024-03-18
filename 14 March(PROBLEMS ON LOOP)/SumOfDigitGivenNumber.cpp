7f057ac736db64735ef73b33a57e25fca7be266d
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-18 14:58:52 +0530

    sum digit given number

---
+++ 14 March(PROBLEMS ON LOOP)/SumOfDigitGivenNumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main(){
+    int n,sum=0;            //n=54
+    cout<<"Enter the number of Digit: ";
+    cin>>n;
+
+    while(n>0){
+        sum=sum+n%10;               //sum=0+(54%10=4),sum=4 reminder
+                                    //next time, sum=4+(5%10=5), sum=9 reminder
+        n=n/10;                     //n=54/10.n=5.4,n=5
+    }
+    cout<<"Sum of Digit: "<<sum;       //sum=9
+}
