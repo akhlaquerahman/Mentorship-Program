31397eb9db07a65471022ee7d200b4e0a8d769d2
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-19 23:27:53 +0530

    Number Square Pattern

---
+++ 15 March(Simple Pattern)/NumberSquarePattern.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+    int a=4;
+
+    for(int i=1; i<=4; i++){            //Row
+        for(int j=1; j<=4; j++){        //Colum
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
