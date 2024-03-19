3b81266014cef4d8bb2b45f117f30726071234ea
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-19 23:58:25 +0530

    Triangle Pattern

---
+++ 15 March(Simple Pattern)/triangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+    int a=5;
+
+    for(int i=1; i<=5; i++){            //Row
+        for(int j=1; j<=i; j++){        //Colum
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
