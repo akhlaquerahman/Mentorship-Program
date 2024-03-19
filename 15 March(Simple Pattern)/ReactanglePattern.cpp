f5697a1c6317e7f82335e857a364876883aae249
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-19 22:38:51 +0530

    Rectangle Pattern

---
+++ 15 March(Simple Pattern)/ReactanglePattern.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+    int a=5;
+
+    for(int i=1; i<=5; i++){            //Row
+        for(int j=1; j<=5; j++){        //Colum
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
