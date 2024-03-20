7d77110e44514aa37811ce694eb0682f8299dbd5
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-20 17:30:56 +0530

    Number Triangle Pattern

---
+++ 16 March (Pattern)/NumberTriangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,number=1;
+    
+    cout<<" Enter the Number: ";
+    cin>>n;
+    
+    for(int i=1; i<=n; i++){        //Row
+       for(int j=1; j<=i; j++){     //Colum
+           cout<<j<<" ";
+       }
+      cout<<endl;
+    }
+    
+    return 0;
+}
