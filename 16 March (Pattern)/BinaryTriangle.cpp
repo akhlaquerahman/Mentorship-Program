630a2f0df4f7af9d932f19f4e2e2e1c34dc4e099
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-20 17:28:54 +0530

    Binary Triangle Pattern

---
+++ 16 March (Pattern)/BinaryTriangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,number=1;
+    
+    cout<<" Enter the Number: ";
+    cin>>n;
+    
+    for(int i=1; i<=n; i++){        //Row
+       for(int j=1; j<=i; j++){     //Colum
+           if((i+j)%2==0){
+           cout<<"1";
+           }
+           else{
+           cout<<"0";
+           }
+           cout<<" ";
+       }
+      cout<<endl;
+    }
+    
+    return 0;
+}
