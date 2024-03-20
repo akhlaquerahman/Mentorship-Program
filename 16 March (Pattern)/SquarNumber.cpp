eb81ec81788f85b492a0e09592a0c3dfdbc32f8e
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-20 17:32:45 +0530

    Squar Number Pattern

---
+++ 16 March (Pattern)/SquarNumber.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,number=1;
+    
+    cout<<" Enter the Number: ";
+    cin>>n;
+    
+    for(int i=1; i<=n; i++){        //Row
+       for(int j=1; j<=n; j++){     //Colum
+           cout<<number<<" ";
+       }
+       number=number+1;
+      cout<<endl;
+    }
+    
+    return 0;
+}
