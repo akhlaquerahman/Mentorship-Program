1f2b5178c15821e261a18bc05ffacbee590b126b
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-20 17:17:56 +0530

    Alpha Squar Pattern

---
+++ 15 March(Simple Pattern)/AlphaberSquar.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main() {
+    int n;
+    cout<<" Enter the Number: ";
+    cin>>n;
+    
+    for(int i=1; i<=n; i++){        //Row
+       for(int j=1; j<=n; j++){     //Colum
+           cout<<char(j+64)<<" ";   //char(j+64)=char(1+64)=65(A)
+       }
+      cout<<endl;
+    }
+    
+}
