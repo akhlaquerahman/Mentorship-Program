0e45a41e5aeed24b50c1ff7b6e7ed84710e3bc58
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-22 22:04:39 +0530

    Find The Product

---
+++ 18 March(Pointer)/FindTheProduct.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int FindTheProduct(int *a,int *b){
+    return (*a)*(*b);
+}
+
+main(){
+    int num1,num2;
+    cout<<"Enter The First Number: ";
+    cin>>num1;
+    cout<<"Enter The Second Number: ";
+    cin>>num2;
+
+    int *ptr1=&num1;
+    int *ptr2=&num2;
+
+    int product= FindTheProduct(ptr1,ptr2);
+
+    cout << "Product of " << *ptr1 << " and " << *ptr2 << " is: " << product << endl;
+
+}
