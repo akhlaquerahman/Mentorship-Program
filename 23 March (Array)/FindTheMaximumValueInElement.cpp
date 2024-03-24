5332f11e601cbc61ddd206a862d8d50701038edd
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-24 23:02:06 +0530

    Maximum Value in Array Element

---
+++ 23 March (Array)/FindTheMaximumValueInElement.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+    int a[5]={12,15,18,24,14};
+    int max=a[0];
+
+    for(int i=0; i<=5; i++){
+        if(a[i]>=max){
+            max=a[i];
+        }
+    }
+    cout<<"Your Maximum Value in index: "<<max;
+}
