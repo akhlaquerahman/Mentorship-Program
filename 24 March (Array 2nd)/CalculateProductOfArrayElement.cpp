dcbfdf5c708c97a03d54049658134707f7d44f52
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-25 03:43:24 +0530

    Product of Element  in Array

---
+++ 24 March (Array 2nd)/CalculateProductOfArrayElement.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+    int a[5]={6,8,5,4,9};
+    int product=1;
+
+    for(int i=0; i<5; i++){
+        product=product*a[i];
+    }
+    cout<<"The Product Of This Element: "<<product;
+}
