35a2226e1b651199ea7c73874271912b347245c4
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-25 03:56:11 +0530

    Minimum value of Element  in Array

---
+++ 24 March (Array 2nd)/FindTheMinimumValueOfArray.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+    int a[5]={15,14,20,24,18};
+    int min=a[0];
+
+    for(int i=0; i<5; i++){
+        if(a[i]<min){
+            min=a[i];
+        }
+    }
+    cout<<"The Minimum Value Of Array: "<<min;
+}
