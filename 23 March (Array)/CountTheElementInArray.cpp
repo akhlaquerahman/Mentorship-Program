31efba2a4cc01a323e9d00935c42bee466d93a00
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-03-24 23:11:12 +0530

    Total Element  in Array

---
+++ 23 March (Array)/CountTheElementInArray.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[5]={12,15,18,24,14};
+    int count=sizeof(a)/sizeof(0);
+    cout<<"The Total Element in Array: "<<count;
+    
+    return 0;
+}
