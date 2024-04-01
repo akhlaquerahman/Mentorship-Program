fd0412070ab67157e5d0e7dece69c23b94e4bb03
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-01 23:30:42 +0530

    Find the Minimum Of The  element:

--- 24 March (Array 2nd)/FindTheMinimumValueOfArray.cpp
+++ 24 March (Array 2nd)/FindTheMinimumValueOfArray.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
- int main(){
+  main(){
     int a[5]={15,14,20,24,18};
     int min=a[0];
 
@@ -10,5 +10,4 @@ using namespace std;
         }
     }
     cout<<"The Minimum Value Of Array: "<<min;
-    return 0;
 }
