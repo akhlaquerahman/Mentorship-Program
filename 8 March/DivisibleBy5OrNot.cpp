82428c87c7064533bdb6f838319490c9885af569
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-02 22:25:51 +0530

    Divisible by 5 or not

--- 8 March/DivisibleBy5OrNot.cpp
+++ 8 March/DivisibleBy5OrNot.cpp
@@ -1,3 +1,5 @@
+                            //Take positive integer input and tell if it is divisible by 5 or not
+
 #include<iostream>
 using namespace std;
 main(){
@@ -5,9 +7,15 @@ main(){
     cout<<"Enter the Number: = ";
     cin>>num;
     if(num/5){
-        cout<<"It is Divisible";
+        cout<<"It is Divisible by 5 :";
     }
     else{
         cout<<"It is not divisible";
     }
 }
+                            /*    Output
+
+                            Enter the Number: = 15
+                            It is Divisible by 5 :
+
+                            */
