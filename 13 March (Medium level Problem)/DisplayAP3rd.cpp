99ceb370f97385270bc08490619ca35f761b943f
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-04 22:30:07 +0530

    Display this AP - 4,7,10,13,16..

--- 13 March (Medium level Problem)/DisplayAP3rd.cpp
+++ 13 March (Medium level Problem)/DisplayAP3rd.cpp
@@ -2,11 +2,19 @@
 #include<iostream>
 using namespace std;
 main(){
-   int a=1,n;
+   int a=4,n;
    cout<<"Enter The Term of AP: ";
    cin>>n;
     for(int i=1; i<=n; i++){
         cout<<a<<endl;
         a+=3;
     }
-}                                
+}                
+                        /*  Output
+                            Enter The Term of AP: 5
+                            4
+                            7
+                            10
+                            13
+                            16
+                        */                
