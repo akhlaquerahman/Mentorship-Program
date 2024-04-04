3a848241ff133998ad8a78484d720d36f605e95b
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-04 22:37:31 +0530

    Display this AP - 100,97,94,..

--- 13 March (Medium level Problem)/DisplayAPpositiveTermUpto100.cpp
+++ 13 March (Medium level Problem)/DisplayAPpositiveTermUpto100.cpp
@@ -7,9 +7,45 @@ main(){
    cout<<"Enter The Term of AP: ";
    cin>>n;
     for(int i=1; i<=n; i++){            //special term follow this condition
-        a-=3;                           //a=>100-3=97,94,91 etc
+        a-=3;                           //a=>100-3=97,94,91 etc.
         if(a>0){
             cout<<a<<endl;
         }
     }
 }
+                        /*  Output
+                            Enter The Term of AP: 40
+                            97
+                            94
+                            91
+                            88
+                            85
+                            82
+                            79
+                            76
+                            73
+                            70
+                            67
+                            64
+                            61
+                            58
+                            55
+                            52
+                            49
+                            46
+                            43
+                            40
+                            37
+                            34
+                            31
+                            28
+                            25
+                            22
+                            19
+                            16
+                            13
+                            10
+                            7
+                            4
+                            1
+                        */
