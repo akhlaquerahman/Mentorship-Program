a43268a61226eec663b2868820a0beffc14e66b9
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-04 22:45:47 +0530

    Predict The Output.

--- 13 March (Medium level Problem)/predictTheOutput.cpp
+++ 13 March (Medium level Problem)/predictTheOutput.cpp
@@ -1,13 +1,14 @@
+                                //Predict The Output.
+
 #include <iostream>
 using namespace std;
 
-int main() {
+main() {
     int j=1;
     while (j <= 10) {
         cout << j << endl;
         j = j + 1;
     }
-    return 0;
 }
                         /*  Output
                             1
