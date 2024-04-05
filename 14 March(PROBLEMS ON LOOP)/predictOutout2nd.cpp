e5f5ae8c71f5ae86510ac0d2b3ec0ba403a282d2
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-05 22:59:19 +0530

    Predict The Output 2

--- 14 March(PROBLEMS ON LOOP)/predictOutout2nd.cpp
+++ 14 March(PROBLEMS ON LOOP)/predictOutout2nd.cpp
@@ -1,6 +1,13 @@
+                            //  Predict output
+
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-while ('1' < '2')
+while ('1' < '2'){
 cout << "In while loop" << endl;
+break;
 }
+}
+            /*  Output
+                In while loop.
+            */
