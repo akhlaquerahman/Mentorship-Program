404edb98764c1b8b12fad579fc5b32ffe98e1888
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-05 21:51:18 +0530

    Count the degit

--- 14 March(PROBLEMS ON LOOP)/factorial.cpp
+++ 14 March(PROBLEMS ON LOOP)/factorial.cpp
@@ -1,3 +1,5 @@
+                            //  Print the factorial of a given number ‘n’
+
 #include<iostream>
 using namespace std;
 main(){
@@ -7,7 +9,11 @@ main(){
 
     for(int i=1; i<=n; i++){
                                 //fact=1,
-        fact=fact*i;                //fact=>i*1=1,fact=>1*1=1,fact=>2*1=2,fact=>3*2=6,fact=>4*6=24,fact=>5*24=120,
+        fact=fact*i;            //fact=>i*1=1,fact=>1*1=1,fact=>2*1=2,fact=>3*2=6,fact=>4*6=24,fact=>5*24=120,
     }
     cout<<"Factorial: = "<<fact;
 }
+                        /*  Output
+                            Enter the number whose factorial is to be Find: 5
+                            Factorial: = 120
+                        */
