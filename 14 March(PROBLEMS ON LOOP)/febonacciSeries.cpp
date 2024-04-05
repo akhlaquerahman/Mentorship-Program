413a0938744d3741fc3206ccd7faad9e535b9634
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-05 22:16:45 +0530

    Find The Febonacci sereis given number

--- 14 March(PROBLEMS ON LOOP)/febonacciSeries.cpp
+++ 14 March(PROBLEMS ON LOOP)/febonacciSeries.cpp
@@ -1,3 +1,5 @@
+                                //Print the Nth fibonacci Number.
+
 #include<iostream>
 using namespace std;
 main(){
@@ -8,13 +10,18 @@ main(){
     cout<<"Enter the Term of Febonacci: ";
     cin>>Term;
     
-    cout<<"Febonaci Series: ";
+    cout<<"Febonaci Series: ";      // 0,1,1,2,3,4,5,6...
     cout<<b<<" ";
 
     for(int i=1; i<=Term; i++){
-        febonaci=a+b;
+        febonaci=a+b;            //febonacci=>0,1,febo.=>0+1=1,febo.=>1+1=2,febo.=>2+1=3,febo.=>3+1=4
+                                 //febonacci=>0,1,1,2,3,4,5,6...
         cout<<febonaci<<" ";
         a=b;
         b=febonaci;
     }
 }   
+                        /*  Output
+                            Enter the Term of Febonacci: 6
+                            Febonaci Series: 0 1 1 2 3 5 8
+                        */
