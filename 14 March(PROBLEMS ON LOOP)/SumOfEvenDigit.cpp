77da96249d8aeb9103191a755e49b044e9a0bd80
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-05 23:16:58 +0530

    Sum Of Even Degit

--- 14 March(PROBLEMS ON LOOP)/SumOfEvenDigit.cpp
+++ 14 March(PROBLEMS ON LOOP)/SumOfEvenDigit.cpp
@@ -1,12 +1,24 @@
+                        //WAP to print the sum of all the even digits of a given number.
+                                //  Sample Input : 4556
+                                //  Output: 10
+
 #include<iostream>
 using namespace std;
 main(){
-    int n=4556,sum=0;           
+    int n=46,sum=0;           
 
     while(n>0){
+        if(n%2==0){
         sum=sum+n%10;               //sum=0+(4556%10=6),sum=6 reminder
                                     //next time, sum=6+(455%10=5), sum=5 reminder
-        n=n/10;                     //n=4556/10
+        n=n/10;
+        }   
+                          //n=4556/10
     }
-    cout<<"Sum of Digit: "<<sum;       //sum=20
+    cout<<"Sum of Even Digit: "<<sum;       //sum=20
 }
+
+                                /*  Output
+                            Enter the number of Digit: 46
+                            Sum of Digit: 10
+                        */     
