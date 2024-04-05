c10b683d71bd4de92e4765c415f70a6343e3f51c
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-05 21:34:07 +0530

    Check prime Number:

---
+++ 14 March(PROBLEMS ON LOOP)/checkNumberPrimeOrNot.cpp
@@ -0,0 +1,31 @@
+                       // WAP to check if a number is prime or not (use of break statement)
+#include<iostream>
+using namespace std;
+main(){
+    int p,q;
+    int PrimeNumber;
+
+    cout<<"Enter The Number Of P: ";
+    cin>>p;
+
+    cout<<"Enter The Number Of Q: ";
+    cin>>q;
+
+            //PrimeNumber=p/q;
+
+    if(q!=0){
+        cout<<"It is Prime Numbers: ";
+    }
+    else{
+        cout<<"It is Not Prime Number: ";
+    }
+}                       
+                        /*  Output
+                        Enter The Number Of P: 4
+                        Enter The Number Of Q: 2
+                        It is Prime Numbers:
+
+                        Enter The Number Of P: 4
+                        Enter The Number Of Q: 0
+                        It is Not Prime Number:
+                        */
--- 14 March(PROBLEMS ON LOOP)/checkNumberPrimeOrNut.cpp
+++
@@ -1,7 +0,0 @@
-                       // WAP to check if a number is prime or not (use of break statement)
-#include<iostream>
-using namespace std;
-main(){
-    int a;
-    
-}                       
