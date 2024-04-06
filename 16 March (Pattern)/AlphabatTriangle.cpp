8489edcebaeb556a28c9b0b4d73dbb48bf23c290
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-06 12:40:36 +0530

    Print alphabet Triangle Pattern

--- 16 March (Pattern)/AlphabatTriangle.cpp
+++ 16 March (Pattern)/AlphabatTriangle.cpp
@@ -1,3 +1,12 @@
+                                    /*  Print the following pattern
+                                        Input: n = 4
+                                        Output:
+                                        A
+                                        A B
+                                        A B C
+                                        A B C D
+                                    */
+
 #include <iostream>
 using namespace std;
 int main() {
