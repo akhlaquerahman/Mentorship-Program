0e81c61cf7da55c4a67d0e3ce0829428d362401d
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-06 11:50:43 +0530

    Odd Number Triangle Pattern

--- 15 March(Simple Pattern)/oddNumberTriangle.cpp
+++ 15 March(Simple Pattern)/oddNumberTriangle.cpp
@@ -1,13 +1,23 @@
+                                    /*  Print the odd Number Triangle pattern
+
+                                            1 
+                                            1 3 
+                                            1 3 5 
+                                            1 3 5 7
+                                    */
+
 #include<iostream>
 using namespace std;
 main(){
-    int a=10;
+    int n;
+    cout<<"Enter The Number: ";
+    cin>>n;
 
-    for(int i=1; i<=a; i++){            //Row
+    for(int i=1; i<=n; i++){            //Row
+        int num=1;
         for(int j=1; j<=i; j++){        //Colum
-            if(j%2!=0){
-            cout<<j<<" ";
-            }
+            cout<<num<<" ";
+            num=num+2;
         }
         cout<<endl;
     }
