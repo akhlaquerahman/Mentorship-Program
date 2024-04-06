99c2c1c5892c9cb0c1a3b48d7e0396d8f78e37ae
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-06 13:12:06 +0530

    Print Number Alphabet triangle Pattern

--- 16 March (Pattern)/NumberAlphabetTriangle.cpp
+++ 16 March (Pattern)/NumberAlphabetTriangle.cpp
@@ -1,3 +1,13 @@
+                                /* Print the following pattern
+                                    Input: n = 4
+                                    Output:
+                                    1
+                                    A B
+                                    1 2 3
+                                    A B C D
+                                    1 2 3 4 5
+                                */
+
 #include <iostream>
 using namespace std;
 int main() {
