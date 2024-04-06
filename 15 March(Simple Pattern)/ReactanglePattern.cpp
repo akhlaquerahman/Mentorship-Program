720195bdce843af87faae6b2bff905dcb5780868
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-06 11:55:13 +0530

    Print Solid Rectangle Pattern

--- 15 March(Simple Pattern)/ReactanglePattern.cpp
+++ 15 March(Simple Pattern)/ReactanglePattern.cpp
@@ -1,11 +1,17 @@
+                                    /*  Print the solid reactangle pattern
+
+                                            *****
+                                            *****
+                                            *****                                    
+                                    */
+
 #include<iostream>
 using namespace std;
 main(){
-    int a=5;
 
-    for(int i=1; i<=5; i++){            //Row
-        for(int j=1; j<=5; j++){        //Colum
-            cout<<"* ";
+    for(int i=1; i<=3; i++){            //Row
+        for(int j=1; j<=7; j++){        //Colum
+            cout<<"*";
         }
         cout<<endl;
     }
