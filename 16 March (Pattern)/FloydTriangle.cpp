692ca1af5a43a9af3fce40f79562f419c5326afc
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-06 13:07:59 +0530

    Print Floyd triangle Pattern

--- 16 March (Pattern)/FloydTriangle.cpp
+++ 16 March (Pattern)/FloydTriangle.cpp
@@ -1,16 +1,23 @@
+                                    /*  PRINT THE FLOYD TRIANGLE PATTERN
+                                        1
+                                        2 3
+                                        4 5 6
+                                        7 8 9 10
+                                    */
+
 #include <iostream>
 using namespace std;
 int main() {
     int n,number=1;
     
-    cout<<" Enter the Number: ";
+    cout<<" Enter the Term: ";
     cin>>n;
     
     for(int i=1; i<=n; i++){        //Row
        for(int j=1; j<=i; j++){     //Colum
            
            cout<<number<<" ";
-           number =number +1;
+           number++;
        }
       cout<<endl;
     }
