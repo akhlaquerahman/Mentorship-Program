a84f7a7afb578b93cccde8b3d980ea27580b72aa
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-06 13:17:27 +0530

    Print Number Reverse triangle Pattern

--- 16 March (Pattern)/ReverseTriangle.cpp
+++ 16 March (Pattern)/ReverseTriangle.cpp
@@ -1,3 +1,12 @@
+                                    /*  Print the following pattern
+                                        Input: n = 4
+                                        Output:
+                                        1 2 3 4
+                                        1 2 3
+                                        1 2
+                                        1
+                                    */
+
 #include <iostream>
 using namespace std;
 int main() {
