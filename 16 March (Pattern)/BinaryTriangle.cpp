0b001e918e729aa79326ecbca08b498aa3983592
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-06 12:46:21 +0530

    Print BINARY Triangle Pattern

--- 16 March (Pattern)/BinaryTriangle.cpp
+++ 16 March (Pattern)/BinaryTriangle.cpp
@@ -1,3 +1,10 @@
+                                    /*  Print the given pattern(BINARY TRIANGLE)
+                                        1
+                                        0 1
+                                        1 0 1
+                                        0 1 0 1
+                                    */
+
 #include <iostream>
 using namespace std;
 int main() {
