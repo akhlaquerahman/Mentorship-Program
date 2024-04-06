83256b69c1056eeee7c95499d330bc5fa03eda5b
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-06 13:03:02 +0530

    Print Cross Pattern

--- 16 March (Pattern)/CrossPattern.cpp
+++ 16 March (Pattern)/CrossPattern.cpp
@@ -1,17 +1,27 @@
+                                    /*  PRINT THE STAR CROSS PATTERN
+                                        *   *
+                                         * *
+                                          *
+                                         * *
+                                        *   *
+                                    */
+
 #include <iostream>
 using namespace std;
 int main() {
-    int n,mid;
+    int n;
     
-    cout<<" Enter the Number: ";
+    cout<<" Enter the Odd Number: ";
     cin>>n;
     
-    for(int i=1; i<=n; i++){        //Row
-       for(int j=1; j<=n; j++){     //Colum
-           if(i==j || (i+j)==(n+1)){
+    for(int i=0; i<n; i++){        //Row
+       for(int j=0; j<n; j++){     //Colum
+           if( i==j || i+j==n-1 ){
            cout<<"*";
-           }   
+           }
+           else{   
            cout<<" ";
+           }
        }
       cout<<endl;
     }
