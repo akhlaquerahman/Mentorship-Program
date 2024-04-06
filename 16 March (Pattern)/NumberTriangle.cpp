9f77bb4b93f34cdb2aa5be07f6ffa6c2ce1ddc9f
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-06 13:14:12 +0530

    Print Number triangle Pattern

--- 16 March (Pattern)/NumberTriangle.cpp
+++ 16 March (Pattern)/NumberTriangle.cpp
@@ -1,3 +1,10 @@
+                                /*  Print the given pattern Number Triangle Flipped
+                                     1
+                                     1 2
+                                     1 2 3
+                                     1 2 3 4
+                                */
+
 #include <iostream>
 using namespace std;
 int main() {
