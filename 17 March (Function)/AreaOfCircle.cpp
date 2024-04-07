f6dbc8b77dd54447ef8160f09ce8e43a3bf48848
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-07 23:44:03 +0530

    Area of circle

--- 17 March (Function)/AreaOfCircle.cpp
+++ 17 March (Function)/AreaOfCircle.cpp
@@ -1,3 +1,5 @@
+            //Q1 : Write a function that takes the radius of a circle as an argument and returns its area.
+
 #include<iostream>
 using namespace std;
 float AreaOfCircle(float radius){
@@ -10,6 +12,10 @@ main(){
     AreaOfCircle(radius);
     cout << "Area of the circle with radius:\n" <<"Radius: = "<< radius << " \nArea Of Circle: =  " <<AreaOfCircle(radius) << endl;
 }
-
+                                /*  Output.
+                                    Area of the circle with radius:
+                                    Radius: = 5 
+                                    Area Of Circle: =  78.5
+                                */
 
 
