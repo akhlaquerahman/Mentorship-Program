cc17a0936a026daac6e39c6da85da39cd1808559
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-16 22:50:17 +0530

    sum Of All Element In 2D Array

---
+++ 29 March (2d Array)/SumofAllElementIn2DArray.cpp
@@ -0,0 +1,32 @@
+        //Write a program to print sum of all the elements of a 2D matrix.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int calculateMatrixSum(const vector<vector<int>>& matrix) {
+    int sum = 0;
+    for (const vector<int>& row : matrix) {
+        for (int element : row) {
+            sum += element;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    // Example 2D matrix
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    // Calculate the sum of all elements
+    int sum = calculateMatrixSum(matrix);
+
+    // Output the result
+    cout << "Sum of all elements of the matrix: " << sum << endl;
+
+    return 0;
+}
+                            /*  Output
+                                Sum of all elements of the matrix: 45
+                            */
