bc668546c9a20a8f5b9857cb3a4b89cc3c03a65c
akhlaquerahman <akhlaquerahman18@gmail.com>
2024-04-21 23:56:18 +0530

    sum Of All Element In 2D Array

--- 29 March (2d Array)/SumofAllElementIn2DArray.cpp
+++ 29 March (2d Array)/SumofAllElementIn2DArray.cpp
@@ -15,7 +15,7 @@ int calculateMatrixSum(const vector<vector<int>>& matrix) {
     return sum;
 }
 
-int main() {
+ main() {
     // Example 2D matrix
     vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
